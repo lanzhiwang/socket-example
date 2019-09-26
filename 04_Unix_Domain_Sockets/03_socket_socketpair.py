3cc8ae5deb28b4e3cb6e7c4a93dc22b1fb004a17
daocloud <daocloud@huzhideMacBook-Pro.local>
2019-09-26 20:02:23 +0800

    Unix_Domain_Sockets

---
+++ 04_Unix_Domain_Sockets/03_socket_socketpair.py
@@ -0,0 +1,22 @@
+import socket
+import os
+
+parent, child = socket.socketpair()
+
+pid = os.fork()
+
+if pid:
+    print('in parent, sending message')
+    child.close()
+    parent.sendall(b'ping')
+    response = parent.recv(1024)
+    print('response from child:', response)
+    parent.close()
+
+else:
+    print('in child, waiting for message')
+    parent.close()
+    message = child.recv(1024)
+    print('message from parent:', message)
+    child.sendall(b'pong')
+    child.close()
